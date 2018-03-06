cb2c18cb46a88a2ca79c2c0458424132749b0ccd
Samuel Yun <shysam.yun@gmail.com>
2018-03-06 10:08:57 -0500

    added "use strict"

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
+'use strict'
+
 let gameOfLife = {
   height: 15,
-  width: 15, // width and height dimensions of the board
-  stepInterval: null, // should be used to hold reference to an interval that is "playing" the game
+  width: 15,
+  stepInterval: null,
 
   createAndShowBoard: function () {
-    // create <table> element
     let goltable = document.createElement("tbody");
-
-    // build Table HTML
     let tablehtml = '';
+
     for (let h = 0; h < this.height; h++) {
       tablehtml += `<tr id='row+${h}'>`;
 
@@ -17,21 +17,18 @@ let gameOfLife = {
       }
       tablehtml += "</tr>";
     }
-    goltable.innerHTML = tablehtml;
 
-    // add table to the #board element
     let board = document.getElementById('board');
+    goltable.innerHTML = tablehtml;
     board.appendChild(goltable);
 
-    // once html elements are added to the page, attach events to them
     this.setupBoardEvents();
   },
 
-
   updateBoard: function () {
     let board = document.getElementById('board');
-
     let newTableHtml = '';
+
     for (let h = 0; h < this.height; h++) {
       newTableHtml += `<tr id='row+${h}'>`;
 
@@ -46,24 +43,9 @@ let gameOfLife = {
     } else {
       board.innerHTML = newTableHtml;
     }
-
   },
 
   forEachCell: function (iteratorFunc) {
-    /*
-      Write forEachCell here. You will have to visit
-      each cell on the board, call the "iteratorFunc" function,
-      and pass into func, the cell and the cell's x & y
-      coordinates. For example: iteratorFunc(cell, x, y)
-    */
-
-    //  for (let h = 0; h < this.height; h++) {
-    //    for (let w = 0; w < this.width; w++) {
-    //      let cell = document.getElementById(`${w}-${h}`);
-    //      iteratorFunc(cell, h, w);
-    //    }
-    //  }
-
     Array.from(document.getElementsByTagName('td')).forEach(cell => {
       let coords = this.getCoordsOfCell(cell);
       iteratorFunc(cell, coords[0], coords[1]);
@@ -125,31 +107,13 @@ let gameOfLife = {
     let gameOfLifeObj = this;
 
     return neighborsArr.filter(function (neighbor) {
-      return gameOfLifeObj.getCellStatus(neighbor) === 'alive'
+      return gameOfLifeObj.getCellStatus(neighbor) === 'alive';
     })
   },
 
   setupBoardEvents: function () {
-    // each board cell has an CSS id in the format of: "x-y"
-    // where x is the x-coordinate and y the y-coordinate
-    // use this fact to loop through all the ids and assign
-    // them "click" events that allow a user to click on
-    // cells to setup the initial state of the game
-    // before clicking "Step" or "Auto-Play"
-
-    // clicking on a cell should toggle the cell between "alive" & "dead"
-    // for ex: an "alive" cell be colored "blue", a dead cell could stay white
-
-    // EXAMPLE FOR ONE CELL
-    // Here is how we would catch a click event on just the 0-0 cell
-    // You need to add the click event on EVERY cell on the board
     let gameOfLifeObj = this;
-
     let onCellClick = function (e) {
-
-      // QUESTION TO ASK YOURSELF: What is "this" equal to here?
-
-      // how to set the style of the cell when it's clicked
       gameOfLifeObj.toggleCellStatus(this);
     }
 
@@ -174,25 +138,14 @@ let gameOfLife = {
       this.width = Number(document.getElementById('column-input').value);
       this.updateBoard();
     });
-
     document.getElementById('default_btn').addEventListener('click', event => {
       this.height = 15;
       this.width = 15;
       this.updateBoard();
     });
-
   },
 
   step: function () {
-    // Here is where you want to loop through all the cells
-    // on the board and determine, based on it's neighbors,
-    // whether the cell should be dead or alive in the next
-    // evolution of the game.
-    //
-    // You need to:
-    // 1. Count alive neighbors for all cells
-    // 2. Set the next state of all cells based on their alive neighbors
-
     let gameOfLifeObj = this;
     let cellsToToggle = [];
 
@@ -224,25 +177,23 @@ let gameOfLife = {
   reset: function () {
     this.forEachCell(cell => {
       let status = Math.random() > 0.5 ? 'alive' : 'dead';
+
       return this.setCellStatus(cell, status);
     });
   },
 
   play: function () {
-    // Start Auto-Play by running the 'step' function
-    // automatically repeatedly every fixed time interval
     if (this.stepInterval) {
       return this.stop();
     }
 
-    this.stepInterval = setInterval(this.step.bind(this), 400);
+    this.stepInterval = setInterval(this.step.bind(this), 350);
   },
 
   stop: function () {
     clearInterval(this.stepInterval);
     this.stepInterval = null;
   },
-
 };
 
 gameOfLife.createAndShowBoard();
